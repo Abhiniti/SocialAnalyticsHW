eecd16ff2ba16fbbbc648ff2f631d3319bb827f3
Abhiniti <abhiniti2004@gmail.com>
2018-08-04 15:54:54 -0400

    HW

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "KCLFeMorOvKCkoowNGcR3w2ZP"
+consumer_secret = "XLBFyILs5BjJK0kNzgCp814hSnNYF4AHHMY40Db4gS7FfGRfra"
+access_token = "4573629617-wTDzl4umBZDZ156uunn0lofCj2uFS75sUBpd1gX"
+access_token_secret = "YNMwNbiZ6x5kQMBs7nFHxzusGOvWH1ZYSTEL91JLfrtbP"
